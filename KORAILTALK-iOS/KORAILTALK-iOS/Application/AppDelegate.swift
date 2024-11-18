a30e6bceced4a182cc4c5470fa327dde223c44ac
joho2022 <josama1997@naver.com>
2024-11-19 02:42:00 +0900

    [Setting] #4 - 네비게이션 바 전역 Appearance 설정
    - AppDelegate에서 네비게이션 바 스타일 중앙 관리

--- KORAILTALK-iOS/KORAILTALK-iOS/Application/AppDelegate.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Application/AppDelegate.swift
@@ -10,10 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        let titleColor = UIColor.white
+        let backgroundColor = UIColor(red: 6/255, green: 59/255, blue: 98/255, alpha: 1)
+        
+        configureNavigationBarAppearance(
+            font: font,
+            titleColor: titleColor,
+            backgroundColor: backgroundColor
+        )
+        
         return true
     }
 
@@ -31,6 +38,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+}
 
+extension AppDelegate {
+    
+    private func configureNavigationBarAppearance(
+        font: UIFont,
+        titleColor: UIColor,
+        backgroundColor: UIColor
+    ) {
+        let appearance = UINavigationBarAppearance()
+        appearance.titleTextAttributes = [
+            .font: font,
+            .foregroundColor: titleColor
+        ]
+        
+        appearance.backgroundColor = backgroundColor
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
 }
 
