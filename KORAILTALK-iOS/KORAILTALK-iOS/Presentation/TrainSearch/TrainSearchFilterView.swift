075425b71a1e43801f043faee1c7b447384900ff
JURI <80569323+juri123123@users.noreply.github.com>
2024-11-19 18:20:55 +0900

    [Feat] #5 열차 filter view UI

--- KORAILTALK-iOS/KORAILTALK-iOS/Presentation/TrainSearch/TrainSearchFilterView.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/TrainSearch/TrainSearchFilterView.swift
@@ -10,8 +10,6 @@ import UIKit
 import SnapKit
 import Then
 
-import SwiftUI
-
 final class TrainSearchFilterView: UIView {
 
     // MARK: - UI Properties
@@ -41,41 +39,81 @@ final class TrainSearchFilterView: UIView {
     
     private func setStyle() {
         
+        var dateContainer = AttributeContainer()
+        dateContainer.font = UIFont.korailTitle(.title3m16)
+        dateContainer.foregroundColor = UIColor.korailBasic(.black)
+        
+        if let resizedImage = UIImage(resource: .icnSearchArrowDown)
+            .resized(CGSize(width: 24, height: 24)) {
+            dateButtonConfig.image = resizedImage
+        } else { return }
+        dateButtonConfig.imagePlacement = .trailing
+        dateButtonConfig.contentInsets = .zero
+        dateButtonConfig.imagePadding = 0
+        //TODO: 날짜 설정 해야 함
+        dateButtonConfig.attributedTitle = AttributedString("2024.11.11 (토)", attributes: dateContainer)
+        
+        var selectContainer = AttributeContainer()
+        selectContainer.font = UIFont.korailBody(.body2m14)
+        selectContainer.foregroundColor = UIColor.korailGrayscale(.gray500)
+        
+        
+        if let resizedImage = UIImage(resource: .icnTrainSearchArrowDown)
+            .resized(CGSize(width: 18, height: 18)) {
+            selectButtonConfig.image = resizedImage
+        } else { return }
+        selectButtonConfig.imagePlacement = .trailing
+        selectButtonConfig.contentInsets = .zero
+        selectButtonConfig.imagePadding = 4
+//        selectButtonConfig.attributedTitle = AttributedString("모든열차", attributes: selectContainer)
+        
+        let selectButtons = [trainSelectButton, seatSelectButton, transferSelectButton]
+        
+        selectButtons.enumerated().forEach { index, button in
+            
+            button.configuration = selectButtonConfig
+            
+            let titles = ["모든열차", "일반석", "직통"]
+            
+            var attributedTitle = AttributedString(titles[index])
+            attributedTitle.font = UIFont.korailBody(.body2m14)
+            attributedTitle.foregroundColor = UIColor.korailGrayscale(.gray500)
+            button.configuration?.attributedTitle = attributedTitle
+        }
+        
         containerStackView.do {
             $0.axis = .horizontal
-//            $0.backgroundColor = .red
+            $0.backgroundColor = UIColor.korailBlue(.blue07)
             $0.alignment = .center
+            $0.distribution = .equalSpacing
             $0.spacing = 27
         }
 
-        
         dateButton.do {
-//            dateButtonConfig.title = "2024.11.11 (토)"
-            $0.
-            dateButtonConfig.image = UIImage(resource: .icnSearchArrowDown)
-            
+            $0.setTitleColor(UIColor.black, for: .normal)
             $0.configuration = dateButtonConfig
         }
         
         selectStackView.do {
             $0.axis = .horizontal
+            $0.distribution = .equalSpacing
             $0.spacing = 8
         }
-        
-        
         
     }
     
     private func setHierachy() {
-        
         addSubview(containerStackView)
         containerStackView.addArrangedSubviews(dateButton, selectStackView)
+        
+        containerStackView.isLayoutMarginsRelativeArrangement = true
+        containerStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 10)
+        
         selectStackView.addArrangedSubviews(
             trainSelectButton,
             seatSelectButton,
             transferSelectButton
         )
-        
     }
     
     private func setLayout() {
@@ -86,16 +124,25 @@ final class TrainSearchFilterView: UIView {
         }
         
         dateButton.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(16)
             $0.verticalEdges.equalToSuperview().inset(12)
+            $0.width.equalTo(129)
         }
         
         selectStackView.snp.makeConstraints {
-            //TODO: 이거 적용이 왜안되묘...
-            $0.trailing.equalToSuperview().inset(10)
             $0.verticalEdges.equalToSuperview().inset(12)
+            $0.width.equalTo(193)
         }
         
-    }
+        trainSelectButton.snp.makeConstraints {
+            $0.width.equalTo(71)
+        }
+        seatSelectButton.snp.makeConstraints {
+            $0.width.equalTo(59)
+        }
+        transferSelectButton.snp.makeConstraints {
+            $0.width.equalTo(47)
+        }
     
+    }
+
 }
---
+++ KORAILTALK-iOS/KORAILTALK-iOS/Presentation/TrainSearch/TrainSelectButton.swift
@@ -0,0 +1,33 @@
+//
+//  TrainSelectButton.swift
+//  KORAILTALK-iOS
+//
+//  Created by 최주리 on 11/19/24.
+//
+
+import UIKit
+
+//final class TrainSelectButton: UIButton {
+//    
+//    private var config = UIButton.Configuration.plain()
+//    
+//    config.image = UIImage(resource: .icnTrainSearchArrowDown)
+//    config.imagePlacement = .trailing
+//    config.imagePadding = 4
+//    
+//    init() {
+//        super.init(frame: .zero)
+//    }
+//    
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//    
+//    init(title: String) {
+//        super.init(frame: .zero)
+//        
+//        setTitle(title, for: .normal)
+//        titleLabel?.font = UIFont
+//    }
+//    
+//}
