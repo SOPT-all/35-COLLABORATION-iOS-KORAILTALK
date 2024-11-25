806d086f7409aefb5a64df60bb60bfbdc37b4bd8
JoHyerin <gpfls468@gmail.com>
2024-11-25 15:18:01 +0900

    [Feat] #11 - 버튼의 이벤트에서는 키보드가 내려가지 않도록 구현

--- KORAILTALK-iOS/KORAILTALK-iOS/Global/Extensions/UIViewController+.swift
+++ KORAILTALK-iOS/KORAILTALK-iOS/Global/Extensions/UIViewController+.swift
@@ -34,9 +34,9 @@ extension UIViewController {
     
     ///키보드 제외한 부분을 탭했을 때 키보드가 내려가는 함수
     func hideKeyboard() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
-                                                                 action: #selector(UIViewController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,  action: #selector(dismissKeyboard))
         tap.cancelsTouchesInView = false
+        tap.delegate = self
         view.addGestureRecognizer(tap)
     }
     
@@ -44,4 +44,15 @@ extension UIViewController {
         view.endEditing(true)
     }
     
+}
+
+extension UIViewController: UIGestureRecognizerDelegate {
+    
+    ///특정 뷰(UIButton)에서 터치 동작을 무시하도록 설정
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        if touch.view is UIButton {
+            return false
+        }
+        return true
+    }
 }
